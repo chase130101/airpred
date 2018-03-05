8a066f055bb5652a4f2bb52497031dfa93a43f64
chase130101 <christopher_hase@g.harvard.edu>
2018-03-05 04:54:54 -0500

    uploading modeling stuff

--- impute/chris_impute_missForest_naOmitMonitorData.R
+++ impute/chris_impute_missForest_naOmitMonitorData.R
@@ -1,10 +1,49 @@
-require('missForest')
-
 # read in data
-sensor.census.to.impute = read.csv('subet_sensor_census_readyToImpute.csv')
+sensor_census = read.csv('../data/subset_sensor_census_toImpute.csv')
+
+# get correlation matrix with for all variables
+corr.mat = as.matrix(cor(sensor_census, use = 'pairwise.complete.obs')) - diag(ncol(sensor_census))
+
+# get variable names for variables with high correlations 
+high.corr.colnames = colnames(corr.mat)[apply(corr.mat, 1, max) >= 0.9]
+
+# columns to delete
+to.delete = c('USElevation_dsc10000','USElevation_max100','USElevation_max10000','USElevation_mea10000','USElevation_med100','USElevation_med10000','USElevation_min100',
+              'USElevation_min10000','USElevation_bln100','USElevation_bln10000', 'NLCD_Developed10000', 'NLCD_Impervious10000', 'MAIACUS_Optical_Depth_055_Aqua_Nearest4', 
+              'MAIACUS_Optical_Depth_055_Terra_Nearest4', 'REANALYSIS_shum_2m_DailyMax', 'REANALYSIS_prate_DailyMax', 'REANALYSIS_prate_DailyMean', 'REANALYSIS_dlwrf_DailyMean',
+              'REANALYSIS_shum_2m_DailyMin', 'REANALYSIS_shum_2m_1Day', 'REANALYSIS_air_sfc_DailyMin', 'REANALYSIS_air_sfc_DailyMax', 'REANALYSIS_air_sfc_DailyMean',
+              'Nearby_Peak2_MaxTemperature', 'Nearby_Peak2_MinTemperature', 'Nearby_Peak2Lag1_MaxTemperature', 'Nearby_Peak2Lag1_MeanTemperature', 'Nearby_Peak2Lag1_MinTemperature', 
+              'Nearby_Peak2Lag3_MaxTemperature', 'Nearby_Peak2Lag3_MinTemperature')
+
+# columns to keep that have high correlations by initial analysis
+high.corr.colnames.del = high.corr.colnames[!(high.corr.colnames %in% to.delete)]
+
+# to inspect which variables have high correlations
+#corr.mat[high.corr.colnames.del, high.corr.colnames.del]
+
+# all columns to keep
+cols.to.keep = names(sensor_census)[!(names(sensor_census) %in% to.delete)]
+
+# final data for imputation
+sensor_census = sensor_census[, cols.to.keep]
+
+# delete rows with monitor data NA
+sensor_census = sensor_census[!is.na(sensor_census$MonitorData),]
+
+# get dataframe with site and monitor data
+site_MonitorData = sensor_census[, c('site', 'MonitorData')]
+
+sensor_census = sensor_census[, !(names(sensor_census) %in% c('site', 'MonitorData'))]
 
 # imputing with random forest
-sensor.census.imputed.rf = missForest(sensor.census.to.impute, maxiter = 10, ntree = 1, variablewise = T,
-                                      decreasing = F, verbose = T, mtry = floor(sqrt(ncol(sensor.census.to.impute))), replace = TRUE)
+require('missForest')
+sensor_census_imputed_rf = missForest(sensor_census, maxiter = 10, ntree = 10, variablewise = T,
+                                      decreasing = F, verbose = T, mtry = floor(sqrt(ncol(sensor_census))), replace = TRUE)
 
-sensor.census.imputed.rf
+# add site and MonitorData back in
+sensor_census_imp = cbind(site_MonitorData, sensor_census_imputed_rf$ximp)
+
+# write to csv for modeling in python
+write.csv(sensor_census_imp, '../data/sensor_census_imputed_rf.csv', row.names = F)
+
+
