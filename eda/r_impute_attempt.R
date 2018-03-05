fdae10909e7ebd411d094aae21fce0e9843577f6
chase130101 <christopher_hase@g.harvard.edu>
2018-03-04 20:19:51 -0500

    committing zipped data file ready for imputation and other zipped data

--- eda/r_impute_attempt.R
+++ eda/r_impute_attempt.R
@@ -1,14 +1,14 @@
 require('mice')
-require('ImputeRobust')
-require('lattice')
 
 # read in data
 sensor.census.to.impute = read.csv('subet_sensor_census_readyToImpute.csv')
-#sensor.census.to.impute2 = sensor.census.to.impute[, -((ncol(sensor.census.to.impute)-11):ncol(sensor.census.to.impute))]
-
 
 # Create the imputed data sets
-imputed.sets = mice(sensor.census.to.impute[1:50000, ], m = 1, maxit = 1,
-                    method = rep('rf', ncol(sensor.census.to.impute)),
+imputed.sets = mice(sensor.census.to.impute[1:10000,], m = 1, maxit = 1,
+                    method = 'mean', ntree = 10,
                     visitSequence = 'monotone',
                     seed = 97123)
+
+missForest(sensor.census.to.impute[1:10000,], maxiter = 10, ntree = 10, variablewise = T,
+           decreasing = F, verbose = T,
+           mtry = floor(sqrt(ncol(sensor.census.to.impute))), replace = TRUE)
