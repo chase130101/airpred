21dde210e8b6c3266fecbe7041e3b455871529d8
Justin Lee <jslee@rclogin07.rc.fas.harvard.edu>
2018-04-08 19:01:13 -0400

    Added output file command line argument, and save to CSV functionality

--- impute/msforest_python.py
+++ impute/msforest_python.py
@@ -1,17 +1,24 @@
 import argparse
 from predictive_imputer import predictive_imputer
+import numpy as np
 import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", help = "Path of input file")
+parser.add_argument("output_file", help = "Path of output file")
 args = parser.parse_args()
 
 data = pd.read_csv(args.filename)
 
-to_remove = ["site", "date", "MonitorData"]
+to_remove = ["site", "date", "MonitorData"]  
+# edit this to drop columns
 
-data = data.drop(to_remove, axis = 1) # drop columns
+cols = [n for n in to_remove if n in data.columns]
+
+data = data.drop(cols, axis = 1) # drop columns as applicable
 
 impute = predictive_imputer.PredictiveImputer()
 impute.fit(data)
-print(impute.transform(data.as_matrix()))
+np.savetxt(args.output_file, impute.transform(data.as_matrix()), delimiter=",")
+
+
