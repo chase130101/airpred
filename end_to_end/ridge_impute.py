0e69a924458ea5d99d81dec52d351961e9aba373
chase130101 <christopher_hase@g.harvard.edu>
2018-04-14 20:16:48 -0400

    deleting slurm files, chaning imputation scripts so that algorithm only trained on subset of read in data

--- end_to_end/ridge_impute.py
+++ end_to_end/ridge_impute.py
@@ -6,38 +6,54 @@ import pickle
 
 np.random.seed(1)
 
-data = pd.read_csv('../data/data_to_impute.csv')
+data = pd.read_csv('../data/data_to_impute.csv', nrows=100000)
 
 train, test = train_test_split(data, train_prop = 0.8, site_var_name = 'site')
+train1, train2 = train_test_split(train, train_prop = 0.3, site_var_name = 'site')
 
-train_x, train_y, train_sites = X_y_site_split(train, y_var_name='MonitorData', site_var_name='site')
+train1_x, train1_y, train1_sites = X_y_site_split(train1, y_var_name='MonitorData', site_var_name='site')
+train2_x, train2_y, train2_sites = X_y_site_split(train2, y_var_name='MonitorData', site_var_name='site')
 test_x, test_y, test_sites = X_y_site_split(test, y_var_name='MonitorData', site_var_name='site')
 
 #https://ianlondon.github.io/blog/encoding-cyclical-features-24hour-time/
-train_x['sin_time'] = np.sin(2*np.pi*train_x.month/12)
-train_x['cos_time'] = np.cos(2*np.pi*train_x.month/12)
+train1_x['sin_time'] = np.sin(2*np.pi*train1_x.month/12)
+train1_x['cos_time'] = np.cos(2*np.pi*train1_x.month/12)
+train2_x['sin_time'] = np.sin(2*np.pi*train2_x.month/12)
+train2_x['cos_time'] = np.cos(2*np.pi*train2_x.month/12)
 test_x['sin_time'] = np.sin(2*np.pi*test_x.month/12)
 test_x['cos_time'] = np.cos(2*np.pi*test_x.month/12)
 
-train_x = train_x.drop(['date', 'month'], axis=1)
+train1_x = train1_x.drop(['date', 'month'], axis=1)
+train2_x = train2_x.drop(['date', 'month'], axis=1)
 test_x = test_x.drop(['date', 'month'], axis=1)
 
 ridge_imputer = PredictiveImputer(max_iter=10, initial_strategy='mean', f_model='Ridge')
-ridge_imputer.fit(train_x, alpha=0.01, fit_intercept=True, normalize=True, tol=0.001, random_state=1)
+ridge_imputer.fit(train1_x, alpha=0.01, fit_intercept=True, normalize=True, tol=0.001, random_state=1)
 
-train_x_imp = ridge_imputer.transform(train_x)
+train1_x_imp = ridge_imputer.transform(train1_x)
+train2_x_imp = ridge_imputer.transform(train2_x)
 test_x_imp = ridge_imputer.transform(test_x)
 
-cols = ['site', 'MonitorData'] + list(train_x.columns)
-train_imp_df = pd.DataFrame(np.concatenate([train_sites.values.reshape(len(train_sites), -1),\
-                                              train_y.values.reshape(len(train_y), -1),\
-                                              train_x_imp], axis=1),\
+cols = ['site', 'MonitorData'] + list(train1_x.columns)
+train1_imp_df = pd.DataFrame(np.concatenate([train1_sites.values.reshape(len(train1_sites), -1),\
+                                              train1_y.values.reshape(len(train1_y), -1),\
+                                              train1_x_imp], axis=1),\
+                                              columns = cols)
+
+train2_imp_df = pd.DataFrame(np.concatenate([train2_sites.values.reshape(len(train2_sites), -1),\
+                                              train2_y.values.reshape(len(train2_y), -1),\
+                                              train2_x_imp], axis=1),\
                                               columns = cols)
 
 test_imp_df = pd.DataFrame(np.concatenate([test_sites.values.reshape(len(test_sites), -1),\
                                               test_y.values.reshape(len(test_y), -1),\
                                               test_x_imp], axis=1),\
                                               columns = cols)
+
+train_imp_df = pd.concat([train1_imp_df, train2_imp_df])
+train_imp_df = train_imp_df.reset_index().sort_values(['site', 'index'])
+train_imp_df.drop('index', axis=1, inplace=True)
+train_imp_df.reset_index(inplace=True, drop=True)
 
 train_imp_df.to_csv('../data/train_ridgeImp.csv', index = False)
 test_imp_df.to_csv('../data/test_ridgeImp.csv', index = False)
