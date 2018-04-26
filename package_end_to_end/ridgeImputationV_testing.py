1637ec37a95b230f9436ca72df2a332452383f4e
justinlee <jsl2239@columbia.edu>
2018-04-25 23:10:58 -0400

    Added config file for file paths in code

---
+++ package_end_to_end/ridgeImputationV_testing.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+train = pd.read_csv('../data/trainV_ridgeImp.csv')
+val = pd.read_csv('../data/valV_ridgeImp.csv')
+test = pd.read_csv('../data/testV_ridgeImp.csv')
+
+print('R^2 (MonitorData ~ MAIACUS_Optical_Depth_047_Terra_Nearest4, train): ' + str((train.loc[:, ('MAIACUS_Optical_Depth_047_Terra_Nearest4', 'MonitorData')].corr()**2).iloc[1,0]))
+print('R^2 (MonitorData ~ MAIACUS_Optical_Depth_047_Terra_Nearest4, val): ' + str((val.loc[:, ('MAIACUS_Optical_Depth_047_Terra_Nearest4', 'MonitorData')].corr()**2).iloc[1,0]))
+print('R^2 (MonitorData ~ MAIACUS_Optical_Depth_047_Terra_Nearest4, test): ' + str((test.loc[:, ('MAIACUS_Optical_Depth_047_Terra_Nearest4', 'MonitorData')].corr()**2).iloc[1,0]))
