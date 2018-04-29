db2c1d3a276e668af1ddbc236dcde421c07d6041
Christopher Hase <chase130101@rclogin04.rc.fas.harvard.edu>
2018-04-29 13:54:46 -0400

    adding bash scripts for running different code scripts

--- end_to_end/ridge-impute-tvt.sh
+++ end_to_end/ridge-impute-tvt.sh
@@ -4,6 +4,6 @@
 #SBATCH -n 1 # number of cores
 #SBATCH --mem 100000 # memory pool for all cores
 #SBATCH -t 0-02:00 # time (D-HH:MM)
-#SBATCH -o slurm.ridgeImputeTT.%N.%j.out # STDOUT
-#SBATCH -e slurm.ridgeImputeTT.%N.%j.err # STDERR
-python ridge_impute_eval_trainTest.py
+#SBATCH -o slurm.ridgeImputeTVT.%N.%j.out # STDOUT
+#SBATCH -e slurm.ridgeImputeTVT.%N.%j.err # STDERR
+python ridge_impute_eval_trainValTest.py
--- end_to_end/xgboost-crossVal.sh
+++ end_to_end/xgboost-crossVal.sh
@@ -4,6 +4,6 @@
 #SBATCH -n 20 # number of cores
 #SBATCH --mem 100000 # memory pool for all cores
 #SBATCH -t 0-05:00 # time (D-HH:MM)
-#SBATCH -o slurm.rfImputeTVT.%N.%j.out # STDOUT
-#SBATCH -e slurm.rfImputeTVT.%N.%j.err # STDERR
-python rf_fit_impute_eval_trainValTest.py
+#SBATCH -o slurm.xgboostCrossVal.%N.%j.out # STDOUT
+#SBATCH -e slurm.xgboostCrossVal.%N.%j.err # STDERR
+python xgboost_crossVal.py
