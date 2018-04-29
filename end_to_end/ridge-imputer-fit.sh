ee7fabf5d0eaaaa793b7a8ccdaa0e12cf66ae5f1
Christopher Hase <chase130101@rclogin07.rc.fas.harvard.edu>
2018-04-29 17:13:21 -0400

    lessening time on imputer fit

--- end_to_end/ridge-imputer-fit.sh
+++ end_to_end/ridge-imputer-fit.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1 # number of nodes
 #SBATCH -n 1 # number of cores
 #SBATCH --mem 100000 # memory pool for all cores
-#SBATCH -t 0-04:00 # time (D-HH:MM)
+#SBATCH -t 0-02:00 # time (D-HH:MM)
 #SBATCH -o slurm.ridgeimputerFit.%N.%j.out # STDOUT
 #SBATCH -e slurm.ridgeimputerFit.%N.%j.err # STDERR
 python ridge_imputer_fit.py
