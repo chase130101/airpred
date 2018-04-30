5098c34352a4566f7ebf774d52d8e3d37fd8194f
chase130101 <christopher_hase@g.harvard.edu>
2018-04-30 17:22:13 -0400

    adding comments to scripts

--- end_to_end/CNN_trainValTest.py
+++ end_to_end/CNN_trainValTest.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-import sklearn.preprocessing
-import sklearn.metrics
 import torch
 from torch.autograd import Variable
+import sklearn.preprocessing
+import sklearn.metrics
 from data_split_tune_utils import X_y_site_split
 from CNN_utils import split_sizes_site, split_data, pad_stack_splits, get_monitorData_indices, r2, get_nonConst_vars
 from CNN_architecture import CNN 
