326b12cd3af383d619b9a5c7abe52e67ba9b230b
chase130101 <christopher_hase@g.harvard.edu>
2018-04-30 01:50:37 -0400

    adding some cross-val results, pickled dicts from crossVal, final ridge model

--- end_to_end/rf-crossVal.sh
+++ end_to_end/rf-crossVal.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1 # number of nodes
 #SBATCH -n 25 # number of cores
 #SBATCH --mem 100000 # memory pool for all cores
-#SBATCH -t 0-12:00 # time (D-HH:MM)
+#SBATCH -t 0-02:00 # time (D-HH:MM)
 #SBATCH -o slurm.rfCrossVal.%N.%j.out # STDOUT
 #SBATCH -e slurm.rfCrossVal.%N.%j.err # STDERR
 python rf_crossVal.py
