02224edfa490d59df0dced910e6c0c41ba5e833b
chase130101 <christopher_hase@g.harvard.edu>
2018-04-30 14:34:01 -0400

    pushing rf feature importances, modified slurms

--- end_to_end/ridge-imputer-fit.sh
+++ end_to_end/ridge-imputer-fit.sh
@@ -4,6 +4,6 @@
 #SBATCH -n 1 # number of cores
 #SBATCH --mem 100000 # memory pool for all cores
 #SBATCH -t 0-02:00 # time (D-HH:MM)
-#SBATCH -o slurm.ridgeimputerFit.%N.%j.out # STDOUT
-#SBATCH -e slurm.ridgeimputerFit.%N.%j.err # STDERR
+#SBATCH -o slurm.ridgeImputerFit.%N.%j.out # STDOUT
+#SBATCH -e slurm.ridgeImputerFit.%N.%j.err # STDERR
 python ridge_imputer_fit.py
