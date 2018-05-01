90c1b7e03ec992adf5e9f5c0764e3a7c7f9de1de
chase130101 <christopher_hase@g.harvard.edu>
2018-04-30 20:40:59 -0400

    adding comments to scripts

--- end_to_end/ridge_crossVal.py
+++ end_to_end/ridge_crossVal.py
@@ -9,11 +9,20 @@ np.random.seed(1)
 
 train = pd.read_csv('../data/train_ridgeImp.csv')
 #train = pd.read_csv('../data/train_rfImp.csv')
+
+# drop rows with no monitor data response value
 train = train.dropna(axis=0)
 
+# model to tune in cross-validation
 ridge = sklearn.linear_model.Ridge(random_state=1, normalize=True, fit_intercept=True)
+
+# hyper-parameters to test in cross-validation
 parameter_grid_ridge = {'alpha' : [0.1, 0.01, 0.001, 0.0001, 0.00001]}
+
+# run cross-validation
 cv_r2, best_hyperparams = cross_validation(data=train, model=ridge, hyperparam_dict=parameter_grid_ridge, num_folds=4, y_var_name='MonitorData', site_var_name='site')
 print('Cross-validation R^2: ' + str(cv_r2))
 print('Best hyper-parameters: ' + str(best_hyperparams))
+
+# save dictionary with best hyper-parameter combination
 pickle.dump(best_hyperparams, open('best_ridge_hyperparams.pkl', 'wb'))
--- end_to_end/xgboost_crossVal.py
+++ end_to_end/xgboost_crossVal.py
@@ -9,16 +9,22 @@ np.random.seed(1)
 
 train = pd.read_csv('../data/train_ridgeImp.csv')
 # train = pd.read_csv('../data/train_rfImp.csv')
+
+# drop rows with no monitor data response value
 train = train.dropna(axis=0)
 
-
+# model to tune in cross-validation
 xgboost = xgb.XGBRegressor(random_state=1, n_jobs=-1)
 
 #Information on gradient boosting parameter tuning
 #https://machinelearningmastery.com/configure-gradient-boosting-algorithm/
+# hyper-parameters to test in cross-validation
 parameter_grid_xgboost = {'learning_rate': [0.001, 0.01, 0.05, 0.1], 'max_depth': [4, 6, 8, 10], 'n_estimators': [100, 250, 500, 750, 1000]}
 
+# run cross-validation
 cv_r2, best_hyperparams = cross_validation(data=train, model=xgboost, hyperparam_dict=parameter_grid_xgboost, num_folds=4, y_var_name='MonitorData', site_var_name='site')
 print('Cross-validation R^2: ' + str(cv_r2))
 print('Best hyper-parameters: ' + str(best_hyperparams))
+
+# save dictionary with best hyper-parameter combination
 pickle.dump(best_hyperparams, open('best_xgboost_hyperparams.pkl', 'wb'))
