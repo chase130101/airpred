ca8cbe3d0ee77c1b722194003e9ffc6b7923b1bc
chase130101 <christopher_hase@g.harvard.edu>
2018-05-01 14:36:12 -0400

    pushing second cnn architecture and valdidation for CNN2

--- end_to_end/CNN_architecture.py
+++ end_to_end/CNN_architecture.py
@@ -53,7 +53,7 @@ class CNN1(torch.nn.Module):
     
     
 class CNN2(torch.nn.Module):
-    def __init__(self, input_size_conv, hidden_size_conv, kernel_size, padding, input_size_full, hidden_size_full, dropout_full, hidden_size_combo, dropout_combo):
+    def __init__(self, input_size_conv, hidden_size_conv, kernel_size, padding, input_size_full, hidden_size_full, dropout_full, hidden_size2_full, dropout2_full):
         super(CNN2, self).__init__()
         
         # convolutional layer for non-constant input variables within a site sequence; normalize output before ReLU; dropout after ReLU
@@ -65,17 +65,15 @@ class CNN2(torch.nn.Module):
         self.linear_full = torch.nn.Linear(in_features=input_size_full, out_features=hidden_size_full, bias=True)
         self.norm_full = torch.nn.BatchNorm1d(num_features=hidden_size_full)
         self.relu_full = torch.nn.ReLU()
-        self.linear
         self.dropout_full = torch.nn.Dropout(p=dropout_full)
+        self.linear2_full = torch.nn.Linear(in_features=hidden_size_full, out_features=hidden_size2_full, bias=True)
+        self.relu2_full = torch.nn.ReLU()
+        self.dropout2_full = torch.nn.Dropout(p=dropout2_full)
         
         # linear layer for combination of outputs from convolution layer and initial linear layer
         # normalize output before ReLU; dropout after ReLU
         # linear layer for model output
-        self.linear1_combo = torch.nn.Linear(in_features=hidden_size_conv+hidden_size_full, out_features=hidden_size_combo, bias=True)
-        self.norm_combo = torch.nn.BatchNorm1d(num_features=hidden_size_combo)
-        self.relu_combo = torch.nn.ReLU()
-        self.dropout_combo = torch.nn.Dropout(p=dropout_combo)
-        self.linear2_combo = torch.nn.Linear(in_features=hidden_size_combo, out_features=1, bias=True)
+        self.linear_combo = torch.nn.Linear(in_features=hidden_size_conv+hidden_size2_full, out_features=1, bias=True)
         
     def forward(self, input_conv, input_full, y_ind_by_site):
         hidden_conv = self.conv1d(input_conv)
@@ -86,6 +84,9 @@ class CNN2(torch.nn.Module):
         hidden_full = self.norm_full(hidden_full)
         hidden_full = self.relu_full(hidden_full)
         hidden_full = self.dropout_full(hidden_full)
+        hidden_full = self.linear2_full(hidden_full)
+        hidden_full = self.relu2_full(hidden_full)
+        hidden_full = self.dropout2_full(hidden_full)
         
         # get convolution outputs for which there is a response value
         hidden_conv_w_response = []
@@ -95,10 +96,6 @@ class CNN2(torch.nn.Module):
         
         # concatenate convolution outputs for which there is a response value with outputs from initial linear layer
         hidden_conv_w_response__hidden_full = torch.cat([hidden_conv_w_response, hidden_full], dim=1)
-        hidden_combo = self.linear1_combo(hidden_conv_w_response__hidden_full)
-        hidden_combo = self.norm_combo(hidden_combo)
-        hidden_combo = self.relu_combo(hidden_combo)
-        hidden_combo = self.dropout_combo(hidden_combo)
-        output = self.linear2_combo(hidden_combo)
+        output = self.linear_combo(hidden_conv_w_response__hidden_full)
 
         return output
