68d97f334306b98342279a8d77152a6e1cf370c4
chase130101 <christopher_hase@g.harvard.edu>
2018-05-02 00:19:02 -0400

    modified and new slurm files

---
+++ end_to_end/cnn1-tt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH -N 1 # number of nodes
+#SBATCH -n 1 # number of cores
+#SBATCH --mem 128000 # memory pool for all cores
+#SBATCH -t 0-04:00 # time (D-HH:MM)
+#SBATCH -o slurm.cnn1TT.%N.%j.out # STDOUT
+#SBATCH -e slurm.cnn1TT.%N.%j.err # STDERR
+python CNN1_trainTest.py
---
+++ end_to_end/cnn2-tt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -p shared
+#SBATCH -N 1 # number of nodes
+#SBATCH -n 1 # number of cores
+#SBATCH --mem 128000 # memory pool for all cores
+#SBATCH -t 0-04:00 # time (D-HH:MM)
+#SBATCH -o slurm.cnn2TT.%N.%j.out # STDOUT
+#SBATCH -e slurm.cnn2TT.%N.%j.err # STDERR
+python CNN2_trainTest.py
