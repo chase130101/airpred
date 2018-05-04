26a71e57d350bad24e2127f0eb6f8e1003799310
chase130101 <christopher_hase@g.harvard.edu>
2018-05-04 14:20:50 -0400

    changing slurm job file params

--- cnn_validate/cnn210004-validate.sh
+++ cnn_validate/cnn210004-validate.sh
@@ -2,8 +2,8 @@
 #SBATCH -p shared
 #SBATCH -N 1 # number of nodes
 #SBATCH -n 1 # number of cores
-#SBATCH --mem 128000 # memory pool for all cores
-#SBATCH -t 0-30:00 # time (D-HH:MM)
+#SBATCH --mem 100000 # memory pool for all cores
+#SBATCH -t 0-24:00 # time (D-HH:MM)
 #SBATCH -o slurm.cnn210004Val.%N.%j.out # STDOUT
 #SBATCH -e slurm.cnn210004Val.%N.%j.err # STDERR
 python CNN2_validate_100_04.py
