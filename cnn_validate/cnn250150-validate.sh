22b9a43dfe8b3116a9947bae61f5cfcee4746e28
chase130101 <christopher_hase@g.harvard.edu>
2018-05-07 16:55:02 -0400

    cnn results

--- cnn_validate/cnn250150-validate.sh
+++ cnn_validate/cnn250150-validate.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1 # number of nodes
 #SBATCH -n 1 # number of cores
 #SBATCH --mem 100000 # memory pool for all cores
-#SBATCH -t 0-15:00 # time (D-HH:MM)
+#SBATCH -t 0-24:00 # time (D-HH:MM)
 #SBATCH -o slurm.cnn250150Val.%N.%j.out # STDOUT
 #SBATCH -e slurm.cnn250150Val.%N.%j.err # STDERR
 python CNN2_validate_50_150.py
--- end_to_end/cnn2-tt.sh
+++ end_to_end/cnn2-tt.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 1 # number of nodes
 #SBATCH -n 1 # number of cores
 #SBATCH --mem 100000 # memory pool for all cores
-#SBATCH -t 0-24:00 # time (D-HH:MM)
-#SBATCH -o slurm.cnn250TT.%N.%j.out # STDOUT
-#SBATCH -e slurm.cnn250TT.%N.%j.err # STDERR
+#SBATCH -t 0-30:00 # time (D-HH:MM)
+#SBATCH -o slurm.cnn2100TT.%N.%j.out # STDOUT
+#SBATCH -e slurm.cnn2100TT.%N.%j.err # STDERR
 python CNN2_trainTest.py
