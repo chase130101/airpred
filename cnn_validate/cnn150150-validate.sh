9a5e01224354a2246bf37096e0dddb8cfa1839e7
chase130101 <christopher_hase@g.harvard.edu>
2018-05-08 11:57:14 -0400

    cleaning up repo, saved deleted stuff

--- cnn_validate/cnn150150-validate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -p general
-#SBATCH -N 1 # number of nodes
-#SBATCH -n 1 # number of cores
-#SBATCH --mem 100000 # memory pool for all cores
-#SBATCH -t 0-24:00 # time (D-HH:MM)
-#SBATCH -o slurm.cnn150150Val.%N.%j.out # STDOUT
-#SBATCH -e slurm.cnn150150Val.%N.%j.err # STDERR
-python CNN1_validate_50_150.py
--- cnn_validate/cnn2100150-validate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -p general
-#SBATCH -N 1 # number of nodes
-#SBATCH -n 1 # number of cores
-#SBATCH --mem 100000 # memory pool for all cores
-#SBATCH -t 0-24:00 # time (D-HH:MM)
-#SBATCH -o slurm.cnn2100150Val.%N.%j.out # STDOUT
-#SBATCH -e slurm.cnn2100150Val.%N.%j.err # STDERR
-python CNN2_validate_100_150.py
