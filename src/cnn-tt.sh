4bd39c3fc05e803993d4fdc9a8b170b2982abba3
chase130101 <christopher_hase@g.harvard.edu>
2018-05-10 19:07:33 -0400

    slurm scripts

---
+++ src/cnn-tt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -p general
+#SBATCH -N 1 # number of nodes
+#SBATCH -n 1 # number of cores
+#SBATCH --mem 100000 # memory pool for all cores
+#SBATCH -t 0-05:00 # time (D-HH:MM)
+#SBATCH -o slurm.cnnTT.%N.%j.out # STDOUT
+#SBATCH -e slurm.cnnTT.%N.%j.err # STDERR
+python CNN_train_test.py
