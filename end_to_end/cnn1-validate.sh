21b9962ff1233f87ebd7ac929049646feb2a19d3
chase130101 <christopher_hase@g.harvard.edu>
2018-05-10 15:14:56 -0400

    cleaning up repo

--- end_to_end/cnn1-validate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -p shared
-#SBATCH -N 1 # number of nodes
-#SBATCH -n 1 # number of cores
-#SBATCH --mem 128000 # memory pool for all cores
-#SBATCH -t 0-24:00 # time (D-HH:MM)
-#SBATCH -o slurm.cnn1Val.%N.%j.out # STDOUT
-#SBATCH -e slurm.cnn1Val.%N.%j.err # STDERR
-python CNN1_validate.py
--- end_to_end/xgboost-crossVal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -p general
-#SBATCH -N 1 # number of nodes
-#SBATCH -n 64 # number of cores
-#SBATCH --mem 128000 # memory pool for all cores
-#SBATCH -t 0-48:00 # time (D-HH:MM)
-#SBATCH -o slurm.xgboostCrossVal.%N.%j.out # STDOUT
-#SBATCH -e slurm.xgboostCrossVal.%N.%j.err # STDERR
-python xgboost_crossVal.py
